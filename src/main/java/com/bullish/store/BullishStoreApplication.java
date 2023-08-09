aa243633fc378eb709ad50230ff8a1485b7e0663
David Hau <david.hau@zuhlke.com>
2023-08-09 18:53:54 +0800

    init commit

---
+++ src/main/java/com/bullish/store/BullishStoreApplication.java
@@ -0,0 +1,13 @@
+package com.bullish.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullishStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullishStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bullish/store/BullishStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullish.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullishStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
